ef7f918362fc587e2faa4879118c124168e54bcd
AcevedoSFD <61608751+AcevedoSFD@users.noreply.github.com>
2020-03-28 13:48:43 -0600

    Quinta version
    Se ha permitido el acceso a la carpeta "Segundos_archivos"

---
+++ Segundos_archivos/otro.js
@@ -0,0 +1,2 @@
+console.log("Esto no debe subirse");
+console.log("En efecto, esto no se ha hecho xd");
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("holi");
 
 //git commit - agregar comentarios a los cambios
 //git log - ver comentarios ya hechos en las ramas
-//git add. - añadir todos los archivos y cambios que se han hecho
+//git add . - añadir todos los archivos y cambios que se han hecho
 //git status - observar el estado de los cambios
 //git ignore - ignorar archivos o carpetas que no quieres que haya
