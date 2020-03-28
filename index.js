8898cbd10e022d674169b00b5ae90e510d4eb6b3
AcevedoSFD <61608751+AcevedoSFD@users.noreply.github.com>
2020-03-28 13:29:18 -0600

    Primer comentario
    hola xd
    Aqui esta mi primera version
    
    Se añadieron  comandos a git
    Hay un archivo de styles

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("holi");
+
+//git commit - agregar comentarios a los cambios
+//git log - ver comentarios ya hechos en las ramas
+//git add. - añadir todos los archivos y cambios que se han hecho
+//git status - observar el estado de los cambios
